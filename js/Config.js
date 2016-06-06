72bf551d0708c62c4c52683f1986ae831f6b3b37
Sida Wang <sidaw@cs.stanford.edu>
2016-06-06 01:58:59 -0700

    demo changes

--- js/Config.js
+++ js/Config.js
@@ -1,16 +1,17 @@
 "use strict"
 var configs = {};
+configs.URL_Parameters = util.parseQueryString();
 if (configs.SEMPRE_URL==undefined)
     configs.SEMPRE_URL = "http://jonsson.stanford.edu:8400"
-if (util.parseQueryString()["local"]==true) {
+if (configs.URL_Parameters.hasOwnProperty("local")) {
   configs.SEMPRE_URL = "http://localhost:8400";
 }
 configs.debugMode = false;
-if (util.parseQueryString()["debug"]==true) {
+if (configs.URL_Parameters.hasOwnProperty("debug")) {
   configs.debugMode = true;
 }
 configs.bePragmatic = true;
-if (util.parseQueryString()["prag"]==false) {
+if (configs.URL_Parameters.hasOwnProperty("nopragmatics")) {
   configs.bePragmatic = false;
 }
 configs.costPerStep = 3;
