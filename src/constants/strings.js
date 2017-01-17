1e2ee7cbd702fa3fa107427815012ca8abce3e57
Sam Ginn <sam@samginn.com>
2017-01-16 16:38:18 -0800

    New run local commands

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,7 +1,8 @@
 const Strings = {
   DEFAULT_SESSIONID: "deadbeef",
-  TUTORIAL_URL: "https://youtu.be/FvEdK_tUoWM",
-  LOGGER_URL: "http://jonsson.stanford.edu:8408"
+  TUTORIAL_URL: "https://youtu.be/FvEdK_tUoWM"
 }
+
+Strings["LOGGER_URL"] = process.env.REACT_APP_SERVER === "local" ? "localhost:8408" : "http://jonsson.stanford.edu:8408"
 
 export default Strings
