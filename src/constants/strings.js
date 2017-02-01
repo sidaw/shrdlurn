1da7788f978209b09c8349453e9e2197faa3b227
Sam Ginn <sam@samginn.com>
2017-02-01 00:52:42 -0800

    New tutorial link

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,6 +1,6 @@
 const Strings = {
   DEFAULT_SESSIONID: "deadbeef",
-  TUTORIAL_URL: "https://youtu.be/FvEdK_tUoWM"
+  TUTORIAL_URL: "https://www.youtube.com/watch?v=zfuYvBZWj1Q"
 }
 
 Strings["LOGGER_URL"] = process.env.REACT_APP_SERVER === "local" ? "localhost:8403" : "http://jonsson.stanford.edu:8403"
