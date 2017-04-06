b65caf07f42c31f19a310eef39800c6e52577b87
Sida Wang <sidaw@cs.stanford.edu>
2017-04-05 18:06:39 -0700

    correct initial state

--- src/store/world.js
+++ src/store/world.js
@@ -2,7 +2,7 @@ import Constants from "constants/actions"
 import { STATUS } from "constants/strings"
 
 const initialState = {
-  history: [{ text: "initial", value: [{ x: 0, y: 0, z: 0, color: "Fake", names: ["S"] }], formula: "(initial)" }],
+  history: [{ text: "initial", value: [{ x: 0, y: 0, z: 0, color: "fake", names: ["S"] }], formula: "(initial)" }],
   responses: [],
   current_history_idx: -1,
   status: STATUS.TRY,
