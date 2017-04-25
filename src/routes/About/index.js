d8768b71dbde330a01ae7f34c6eb3b64530759ff
Sida Wang <sidaw@cs.stanford.edu>
2017-04-25 01:55:11 -0700

    camera ready

--- src/routes/About/index.js
+++ src/routes/About/index.js
@@ -17,23 +17,29 @@ class Information extends Component {
 		return (
 			<div className="About">
 				<div>
-
 					<img src={CubesImage} alt="example voxel structure" height={height} />
 					<p>Voxelurn is a command interface for building {' '}
 						<Link to={{ pathname: "/community", query: this.props.query }} activeClassName="active" target="_blank">voxel structures</Link>.{' '}
-						It is an experimental platform for developing technologies
-				allowing computers to understand a naturalized programming language, which allows people to access
-				to the power of programming languages
-				without conforming to their uncompromising syntax.{' '}
-						Voxlurn does this by learning from its user community interactively starting from just a normal programming language.
+						It is an experimental platform for developing techniques
+    				allowing computers to parse a naturalized programming language.
+            Our goal is to allow people access
+    				to the power of programming languages
+    				without conforming to their uncompromising syntax.{' '}
+						Voxlurn does this by learning from its user community interactively starting from a precise programming language.
 				</p>
         <h2>Get started</h2>
-        Go to the <Link to="/build">build page</Link> and type "repeat 3 [add red top]".
-      Voxelurn always understands the core language which has a fixed syntax like other programming languages.
-      However, you might want more flexibility and many alternative ways of saying the same thing.
-      In Voxelurn, you can define
-      "add red top 3 times" as "repeat 3 [add red top]" and in the future you can try
-      "add green left 5 times".
+      <ul>
+       <li> Go to the <Link to="/build">build page</Link> and type "repeat 3 [add red top]". </li>
+       <li>Voxelurn always understands the <a target="_blank" href={DOCUMENTATION_URL}>core language</a>, which has a fixed syntax like other programming languages.</li>
+       <li> You can define
+      "add red top 3 times" by clicking the "Define this" button and then enter "repeat 3 [add red top]".
+      You can now use
+      "add green left 5 times". </li>
+      <li>Alternatively, you can use the mouse to select to select some previous commands and define them.</li>
+      <li>See how definitions works in our <a target="_blank" href={TUTORIAL_URL}>overview video</a>.</li>
+      <li>Previous users already defined some concepts and used them to build {' '}
+      <Link to={{ pathname: "/community", query: this.props.query }} activeClassName="active" target="_blank">voxel structures</Link>.</li>
+       </ul>
 					{/*
 					<div className="Examples">
 					<img src={require('./examples.png')} height={height}/>
@@ -42,25 +48,25 @@ class Information extends Component {
 				<img src={require('./struct_venison.png')} height={height}/>
 				</div>
 				*/}
-					<h2>Prizes</h2>
-					<p>
-						We award prizes for users contributing the most useful language, and users building the best structures.
+				{/*
+        <h2>Prizes</h2>
+				<p>
+				We award prizes for users contributing the most useful language, and users building the best structures.
 				Six $50 prizes are handed out every two weeks for the top 3 language teachers and the top 3 best structures.
 				To participate, you have to <a target="_blank" href="https://shrdlurn.signup.team/">join</a> our <a target="_blank" href="https://shrdlurn.slack.com/">slack channel</a> and login.
 				More details on the competition can be found there.
 				</p>
-
-
+        */}
 
 				<h2>Learn more</h2>
-        <div><a target="_blank" href={DOCUMENTATION_URL}>Documentation</a>: core language, the setup, etc </div>
-        <div><a target="_blank" href={TUTORIAL_URL}>Video tutorial</a>: see the system at work</div>
-        <a target="_blank" href={SLACK_SIGNUP_URL}> Slack</a> (<a target="_blank" href="https://shrdlurn.signup.team/">signup</a>): signup and join to submit more structures and win prizes
-        <p>
-        Even more information can be found on <a href="https://github.com/sidaw/shrdlurn">github</a>.
-        </p>
-      </div>
-			</div>
+        <ul>
+        <li><a target="_blank" href={TUTORIAL_URL}>Video overview</a>: see the system at work</li>
+        <li><a target="_blank" href={DOCUMENTATION_URL}>Documentation</a>: core language, the setup, etc </li>
+        <li><a target="_blank" href={SLACK_SIGNUP_URL}> Slack</a> (<a target="_blank" href="https://shrdlurn.signup.team/">signup</a>): signup and join to submit more structures and win prizes</li>
+        <li><a target="_blank" href="https://github.com/sidaw/shrdlurn/blob/master/Voxelurn.md#defined-by-users">Examples</a>: actions and structures defined by users</li>
+        <li><a target="_blank" href="https://arxiv.org/abs/1704.06956">Our techical paper</a></li>
+        </ul>
+        </div></div>
 		)
 	}
 }
